28ebe77a074ee2386e29b9fdb8f4863f0b240308
victor okada <victoryassu@hotmail.com>
2023-04-21 17:10:53 -0300

    Atualizado Parser01

--- parser01/main.c
+++ parser01/main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 #define OPERACOES C(MAIS)C(MENOS)C(MULTIPLICACAO)C(DIVIDIR)C(INDETERMINADO)C(POTENCIA)C(N_INTEIRO)C(N_REAL)
 #define C(x) x,
@@ -11,102 +12,57 @@ enum color { OPERACOES TOP };
 #define C(x) #x,    
 const char * const op[] = { OPERACOES };
 
-int converterCaractere(char letra)
+int tamanho_string(char *pointer)
 {
-  switch (letra) 
-  {
-    case '0': 
-      return 0; 
-      break;
-    
-    case '1': 
-      return 1; 
-      break;
-    
-    case '2': 
-      return 2; 
-      break;
-    
-    case '3': 
-      return 3; 
-      break;
-    
-    case '4': 
-      return 4; 
-      break;
-    
-    case '5': 
-      return 5; 
-      break;
-    
-    case '6': 
-      return 6; 
-      break;
-    
-    case '7': 
-      return 7; 
-      break;
-    
-    case '8': 
-      return 8; 
-      break;
-    
-    case '9': 
-      return 9; 
-      break;
-    
-   default:
-      break;
-  }
+   int c = 0;
+ 
+   while( *(pointer + c) != '\0' )
+      c++;
+ 
+   return c;
+}
+
+char* inverteString(char *s)
+{
+   int length, c;
+   char *begin, *end, temp;
+   length = tamanho_string(s);
+   begin = s;
+   end = s;
+ 
+   for (c = 0; c < length - 1; c++)
+      end++;
+ 
+   for (c = 0; c < length/2; c++)
+   {        
+      temp = *end;
+      *end = *begin;
+      *begin = temp;
+ 
+      begin++;
+      end--;
+   }
+   return s;
+}
+
+int converterPraInteiro(char *letra)
+{
+  int a = atoi(letra); //converte char pra inteiro
+  
+  return a;
   
 }
 
-void SomaInteiro(int num1, int num2)
-{  
-    // int tamanho = 0;
-    // tamanho = sizeof(vetor) / sizeof(int);
-
-    // printf("So, array length is: %d\n", tamanho);
+double converterPraDouble(char *letra)
+{
+  double b = atof(letra); //converte char pra double
   
-    // for (int i = 0; i < tamanho; i++)
-    // {
-    //   printf("Valor da soma: %d\n", vetor[i]);
-    // }
-    printf("%d + %d = ", num1, num2);
-    int result = num1 + num2;
-    printf("%d", result);
+  return b;
   
 }
 
-
-void SomaReal(double num1, double num2)
-{  
-    // int tamanho = 0;
-    // tamanho = sizeof(vetor) / sizeof(int);
-
-    // printf("So, array length is: %d\n", tamanho);
-  
-    // for (int i = 0; i < tamanho; i++)
-    // {
-    //   printf("Valor da soma: %d\n", vetor[i]);
-    // }
-    printf("%lf + %lf = ", num1, num2);
-    double result = num1 + num2;
-    printf("%lf", result);
-  
-}
-// void Subtracao()
-// {
-  
-// }
-// void Multiplicacao()
-// {
-  
-// }
-// void Divisao()
-// {
-  
-// }
+int SomaInteiro(int a, int b) { return a + b; } //funcao de soma de inteiros
+double SomaReal(double a, double b) { return a + b; } //funcao de soma de double
 
 void tokenizar(char *str)
 {
@@ -119,21 +75,61 @@ void tokenizar(char *str)
     const char *resposta7 = op[ N_INTEIRO ];
     const char *resposta8 = op[ N_REAL ];
     int achouPonto = 0;
-    int resultado, resultado2; 
-    int acumulador[100] = {};
-    int contador = 0; 
-    int j = 0; 
-    bool soma = false;
+    double resultado = 0;
+    int resultado2 = 0;
+    int atual = 0;
+    char num1[999] = "";
+    char num2[999] = "";
+    char *invertido = "";
   
     for (int i = 0; i <= strlen(str); i++) 
     {
     
       switch(str[i])
       {
-        
         case '+':
-         // printf(" + "); // imprime MAIS
-          soma = true;
+ 
+          atual = i; 
+          
+          while(str[i - 2] != ' ') //verifica todos os numeros antes do simbolo de + até encontrar espaço vazio
+          {
+            strncat(num1, &str[i - 2], 1); // adiciona caractere ao array num1
+          
+            i = i - 1; //passa pra posicao anterior
+          }
+          
+          i = atual;
+          
+          while(str[i + 2] != ' ') //verifica todos os numeros depois do simbolo de + até encontrar espaço vazio
+          {
+            strncat(num2, &str[i + 2], 1); // adiciona caractere ao array num2
+            
+            i = i + 1; //passa pra posicao posterior
+          }
+
+          invertido = inverteString(num1); //inverte array num1 pra realização de conversão
+
+          if ((strstr(num1, ".") != NULL) || (strstr(num2, ".") != NULL)) //verifica se expressao de array num1 e num2 são do tipo double. se sim trata como double, caso contrario, rata como inteiro
+          {
+              
+          
+            double inteiro1 = converterPraDouble(invertido); // converte expressao invertido pra double
+            double inteiro2 = converterPraDouble(num2); // converte expressao num2 pra double
+            
+            resultado = SomaReal(inteiro1, inteiro2); //soma expressao inteiro1 com inteiro2
+            printf("  Soma -> %.2f,\n", resultado); 
+          }
+          else{
+
+            int n1 = converterPraInteiro(invertido); // converte expressao invertido pra inteiro
+            int n2 = converterPraInteiro(num2); // converte expressao num2 pra inteiro
+             
+            resultado2 = SomaInteiro(n1, n2); //soma expressao n1 com n2
+            printf("  Soma -> %d,\n", resultado2); 
+              
+          }
+      
+          //printf("  %s,\n", resposta1); // imprime MAIS
           break;
         
         case '-':
@@ -144,6 +140,7 @@ void tokenizar(char *str)
       
           if(str[i] == str[i + 1])  printf("  %s,\n", resposta6); // se caractere proximo for igual ao atual, entao imprime POTENCIA
           if(str[i] != str[i + 1])  printf("  %s,\n", resposta3); // se caractere proximo for diferente do caractere atual imprime MULTIPLICACAO
+
           if(str[i + 1] == ' ' || str[i + 1] == '*') i = i + 1; // avanca um caractere para nao haver impressao repetida
           
           break;
@@ -154,54 +151,20 @@ void tokenizar(char *str)
 
         case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '.': 
        
-         // while (str[i] != ' ') 
-          //{
-            //printf("c => %c\n", str[i]);
-            resultado2 = resultado;
-            resultado = converterCaractere(str[i]);
-            contador = contador + 1;
-
-            if(contador == 2) SomaInteiro(resultado2, resultado);
+          while (str[i] != ' ') {
         
-            //if(str[i] == ' ') //achouPonto = achouPonto + 1; // se houver encontrado o caractere ponto, contabiliza a variavel achouPonto
-              //printf("c => %c\n", str[i + 2]);
-              //printf("c => %c\n", str[i + 2]);
-              //resultado = converterCaractere(str[i]);
+            if(str[i] == '.') achouPonto = achouPonto + 1; // se houver encontrado o caractere ponto, contabiliza a variavel achouPonto
   
-              //resultado2 = converterCaractere(str[i + 1]);
-
-              //printf("caractere %d\n", resultado);
-              //printf("caractere2 %d\n", resultado2);
-  
-              //Soma(resultado, resultado2);
-  
-            //acumulador[j] = resultado; /// acumula valores de caracteres na sequencia
-            
-            //if (acumulador[j] > 0) printf("Acumulador %d\n", acumulador[j]);
-            //if(resultado > 0) printf("resultado %d\n", resultado);
-            //printf(soma ? "true\n" : "false\n");
-            //printf("num 1 %d\n", acumulador[j]);
-            //printf("num 2 %d\n", acumulador[j + 2]);
-            
-            //i = i + 2; // avanca um caractere para nao haver contabilização de caractere repetido
-            //j = j + 1;
-   
-         // }
-
-          //printf("Acumulador")
-
-          //j = 0;
+            i = i + 1; // avanca um caractere para nao haver contabilização de caractere repetido
+      
+          }
          
-          //printf("num 1 %d\n", acumulador[j]);
-          //printf("num 2 %d\n", acumulador[j + 2]);
-        
-          
-          //if(achouPonto == 0 && soma == true) Soma(acumulador[j], acumulador[j + 2]);
-            //printf("  %s,\n", resposta7); //Soma(acumulador); // imprime N_INTEIRO pois nao achou ponto
-          //if(achouPonto == 1) printf("  %s,\n", resposta8); // imprime N_REAL pois achou apenas 1 ponto
-          //if(achouPonto > 1) printf("  %s,\n", resposta5); // imprime INDETERMINADO pois achou mais de 1 ponto
+         
+          if(achouPonto == 0) printf("  %s,\n", resposta7); // imprime N_INTEIRO pois nao achou ponto
+          if(achouPonto == 1) printf("  %s,\n", resposta8); // imprime N_REAL pois achou apenas 1 ponto
+          if(achouPonto > 1) printf("  %s,\n", resposta5); // imprime INDETERMINADO pois achou mais de 1 ponto
 
-          //achouPonto = 0; // zera variavel para contabilizar numeros reais novamente
+          achouPonto = 0; // zera variavel para contabilizar numeros reais novamente
           
           break;
         
@@ -244,10 +207,15 @@ int main(void)
 
 }
 
-/* REFERENCIAS
+/* 
+
+REFERENCIAS
 
 https://stackoverflow.com/questions/9907160/how-to-convert-enum-names-to-string-in-c
 https://www.geeksforgeeks.org/c-program-to-extract-characters-from-a-string/
 https://stackoverflow.com/questions/10349842/how-does-c-know-the-end-of-my-string
+https://www.educative.io/answers/how-to-convert-a-string-to-an-integer-in-c
+https://www.programmingsimplified.com/c-program-reverse-string
+https://stackoverflow.com/questions/13424265/convert-a-char-to-double
 
 */
